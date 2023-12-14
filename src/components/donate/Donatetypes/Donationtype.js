3cd148a13e167a6161cbdf13f9e5001f609937ce
Priyesh Gupta <priyeshg.gupta5@gmail.com>
2023-12-15 00:52:10 +0530

    db.json created

--- src/components/donate/Donatetypes/Donationtype.js
+++ src/components/donate/Donatetypes/Donationtype.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Donationtype = ({name,type,date}) => {
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/donationCategories');
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Name is : {name}</h1>
-      <h1>Type is :{type} </h1>
-      <h1>date is : {date}</h1>
+      <h2>Product List</h2>
+      <ul>
+        {products.map(category => (
+          <li key={category.categoryName}>
+            <h3>{category.categoryName}</h3>
+            <ul>
+              {category.donations.map(donation => (
+                <li key={donation.nameEnglish}>
+                  {donation.nameEnglish} - {donation.costPerUnit}
+                </li>
+              ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Donationtype
-
+export default ProductList;
--- src/pages/Donate/Donate.js
+++ src/pages/Donate/Donate.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Donatebtn from '../../components/donate/Donatebtn/Donatebtn'
+import Donatetype from '../../components/donate/Donatetypes/Donationtype'
 
 export default function Donate() {
   return (
     <div>
    <Donatebtn/>
+   <Donatetype/>
     </div>
 
   )
