387638eee21bec2afb2242df1762b53be0cd1d52
Ayush2705 <ayush.agr.27.5@gmail.com>
2024-01-04 00:48:18 +0530

    added popupto yearly and lifetime membership

--- src/components/homepage/Lifetime/Lifetime.js
+++ src/components/homepage/Lifetime/Lifetime.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Lifetime.css";
+import Popup from '../../donate/Popup/Popup';
 
 export default function Lifetime() {
+  const [showPopup, setShowPopup] = useState(false);
+  const handleBecomeMember = () => {
+    const additionalData = {
+      amount: 21000, // Hardcoded amount
+      type: "Membership", // Hardcoded type
+      productName: "Lifetime Membership", // Hardcoded product name
+      units: 1, // Hardcoded units
+    };
+    setShowPopup({...additionalData,show:true});
+  };
   return (
     <div className="mb-5 mt-4 container text-center">
       <h2 className="lifetime-heading">Join us for Lifetime Subscription</h2>
@@ -10,9 +21,10 @@ export default function Lifetime() {
       Empower change with a <strong>₹ 21000</strong> Lifetime Membership for old and sick cows, and receive a special certificate!
       </p>
 
-      <button className="btn btn-success lifetime-btn">
+      <button className="btn btn-success lifetime-btn" onClick={handleBecomeMember}>
         Become a Member
       </button>
+      {showPopup.show && <Popup onClose={() => setShowPopup({ ...showPopup, show: false })} donationInfo={showPopup} />}
     </div>
   );
 }
--- src/components/homepage/Membership/Membership.js
+++ src/components/homepage/Membership/Membership.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Membership.css";
+import Popup from '../../donate/Popup/Popup';
 
 export default function Membership() {
+  const [showPopup, setShowPopup] = useState(false);
+  const handleBecomeMember = () => {
+    const additionalData = {
+      amount: 1100, // Hardcoded amount
+      type: "Membership", // Hardcoded type
+      productName: "Yearly Membership", // Hardcoded product name
+      units: 1, // Hardcoded units
+    };
+    setShowPopup({...additionalData,show:true});
+  };
+  
   return (
     <div className="mb-5 mt-4 container text-center">
       <h2 className="membership-heading">Become a Yearly Member</h2>
@@ -10,9 +22,10 @@ export default function Membership() {
       Empower change with a <strong>₹ 1100</strong> Yearly Membership for old and sick cows, and receive a special certificate!
       </p>
 
-      <button className="btn btn-success membership-btn">
+      <button className="btn btn-success membership-btn" onClick={handleBecomeMember}>
         Become a Member
       </button>
+      {showPopup.show && <Popup onClose={() => setShowPopup({ ...showPopup, show: false })} donationInfo={showPopup} />}
     </div>
   );
 }
