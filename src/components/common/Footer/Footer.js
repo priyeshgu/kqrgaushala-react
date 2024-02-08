733080e0345bc477a1721f04ee7307cf703db169
Priyesh <priyeshg.gupta5@gmail.com>
2024-02-08 20:45:21 +0530

    privacy pages added to footer

--- src/components/common/Footer/Footer.js
+++ src/components/common/Footer/Footer.js
@@ -15,7 +15,10 @@ const Footer = () => {
             <a href='/about' className='footer-links'> <p>About Us</p></a>
             <a href='/contact' className='footer-links'> <p>Contact Us</p></a>
             <a href='/donate' className='footer-links'> <p>Donate</p></a>
-            <a href='/policy' className='footer-links'> <p>Policies | T&C</p></a>
+            <a href='/terms' className='footer-links'> <p>Terms & Conditions</p></a>
+            <a href='/refund' className='footer-links'> <p>Refund Policy</p></a>
+            <a href='/privacy' className='footer-links'> <p>Privacy Policy</p></a>
+           
 
           </div>
 
